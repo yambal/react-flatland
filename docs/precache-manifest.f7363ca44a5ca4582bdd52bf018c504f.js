c3ee660ce4a6c6a68b05c66608f9774d99f0a8c0
June YAMAMOTO <june@yambal.net>
2020-06-07 01:52:46 +0900

    images

---
+++ docs/precache-manifest.f7363ca44a5ca4582bdd52bf018c504f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc471d130ed7b76d4a9519924ec2b220",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f66daa47a500cefd7dd",
+    "url": "./static/js/2.5dc6fb73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.5dc6fb73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be333e3c984ef93adc8b",
+    "url": "./static/js/main.22207d93.chunk.js"
+  },
+  {
+    "revision": "d500b85bf46da5915600",
+    "url": "./static/js/runtime-main.14cbca36.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56828b4c3a0f7ed5ba00baebeacbc477.js"
+  "./precache-manifest.f7363ca44a5ca4582bdd52bf018c504f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
